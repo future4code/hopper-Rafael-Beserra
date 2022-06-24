98946d050836e8459ad0f0f68498368de15cad72
Rafael Santos Beserra <98047856+RafaelSantosBeserra@users.noreply.github.com>
2022-06-24 19:13:29 -0300

    início exercício

---
+++ starWars/star-wars-app/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import CharacterDetailPage from "./componentes/CharacterDetailPage/CharacterDetailPage";
+
+export default function App () {
+
+    return(
+      
+      <div className="App">
+        <header className="App-header">
+          <h1>Star Wars</h1>
+        </header>
+      </div>
+    );
+
+}
+
+// class App extends React.Component {
+
+//   render() {
+
+//   };
+// };
+
+// export default App;
