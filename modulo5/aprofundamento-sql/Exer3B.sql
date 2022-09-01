2023c15ba2bb9037608d4a45a93e7d8da4a0b0b8
RafaelSantosBeserra <rs.beserra@gmail.com>
2022-09-01 00:29:04 -0300

    Aprofundamento SQL

---
+++ modulo5/aprofundamento-sql/Exer3B.sql
@@ -0,0 +1,4 @@
+ALTER TABLE Projetos_Empresas
+CHANGE date dueDate DATE NOT NULL;
+
+DESCRIBE Projetos_Empresas;
---
+++ modulo5/aprofundamento-sql/Exer4A.sql
@@ -0,0 +1,4 @@
+ALTER TABLE Projetos_Empresas
+ADD description VARCHAR(40);
+
+DESCRIBE Projetos_Empresas;
